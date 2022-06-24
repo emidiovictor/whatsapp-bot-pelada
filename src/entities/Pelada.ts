b81d0c7a2029db1a9f6ca537630ae76c90d4c792
Victor Emídio <victoremidiO@yahoo.com.br>
2022-06-24 11:53:33 -0300

    refactor: change responsability of creating pelada to Pelada class

--- src/entities/Pelada.ts
+++ src/entities/Pelada.ts
@@ -1,9 +1,21 @@
 import { NoPermissionToCreatePeladaException } from "./errors/NoPermissionToCreatePeladaException";
+import { NoPermissionToInvitePlayerException } from "./errors/NoPermissionToInvitePlayerException";
+import { NoPermissionToRemovePlayerException } from "./errors/NoPermissionToRemovePlayerException";
 import { PeladaWithWrongDateException } from "./errors/PeladaWithWrongDateException";
 import { PlayerIsAlreadyInThePelada } from "./errors/PlayersIsAlreadyInThePelada";
 import { Player } from "./Player";
 
 class Pelada {
+  invitePlayer(potentialAdemin: Player, player: Player) {
+    if (!potentialAdemin.isAdmin) {
+      throw new NoPermissionToInvitePlayerException();
+    }
+  }
+  kickPlayer(potentialAdemin: Player, playerToRemove: Player) {
+    if (!potentialAdemin.isAdmin) {
+      throw new NoPermissionToRemovePlayerException();
+    }
+  }
   private constructor(
     readonly place: string,
     readonly maxPlayers: number,
--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,19 +1,6 @@
-import { NoPermissionToInvitePlayerException } from "./errors/NoPermissionToInvitePlayerException";
-import { NoPermissionToRemovePlayerException } from "./errors/NoPermissionToRemovePlayerException";
 import Pelada from "./Pelada";
 
 export class Player {
-  removePlayer(potentialAdmin: Player) {
-    if (!this.isAdmin) {
-      throw new NoPermissionToRemovePlayerException();
-    }
-  }
-  invitePlayer(potentialAdmin: Player) {
-    if (!this.isAdmin) {
-      throw new NoPermissionToInvitePlayerException();
-    }
-  }
-
   constructor(
     readonly id: number,
     readonly isAdmin: boolean = false,
