380fcdf3ad1d8771b772b1ff04bfda154a3b7dd9
Victor Emídio <victoremidiO@yahoo.com.br>
2022-06-24 11:09:17 -0300

    refactor: change invite player to player class

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,8 +1,16 @@
+import Pelada from "./Pelada";
+
 export class Player {
+  invitePlayer(player: Player) {
+    if (!this.isAdmin) {
+      throw new Error('Você não tem permissão para convidar um jogador!');
+    }
+  }
 
   constructor(
     readonly id: number,
-    readonly isAdmin: boolean = false) {
+    readonly isAdmin: boolean = false,
+    readonly peladaOwner?: Pelada) {
   }
 }
 
