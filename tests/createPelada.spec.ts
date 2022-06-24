8e6628a957651bccfa32d87c30a8ed0e8139c11a
Victor Emídio <victoremidiO@yahoo.com.br>
2022-06-24 10:59:07 -0300

    refactor: move test case to correct file

--- tests/createPelada.spec.ts
+++ tests/createPelada.spec.ts
@@ -8,17 +8,6 @@ describe('CreatePelada', () => {
     }).toThrowError('Você não tem permissão para criar uma pelada');
   });
 
-  it('should thrown exception when try to add a player that is already in the pelada', () => {
-    var admin = new Player(1, true);
-    var player = new Player(2);
-    var pelada = Pelada.createPelada('place', 20, 10, new Date(), admin);
-    pelada.addPlayer(player)
-    const thrower = pelada;
-    expect(() => {
-      thrower.addPlayer(player);
-    }).toThrowError('O jogador já está na pelada!');
-  });
-
   it('should throw when admin tries to create with date before than today', () => {
     var admin = new Player(1, true);
     expect(() => {
