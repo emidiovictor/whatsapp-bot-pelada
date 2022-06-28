486e95584002cbefc80f1dc44e2e2be08c8e17e6
Victor Emídio <victoremidiO@yahoo.com.br>
2022-06-27 21:40:35 -0300

    refactor: create a factory for the SUT

--- tests/services/PeladaService.spec.ts
+++ tests/services/PeladaService.spec.ts
@@ -1,15 +1,27 @@
+import Pelada from '@/domain/entities/Pelada';
+import Player from '@/domain/entities/Player';
+import { IPeladaRepository } from '@/domain/entities/repositories/IPeladaRepository';
 import { PeladaService } from '@/services/PeladaService';
+
+const createSut = (): PeladaService => {
+	const peladaRepository = {
+		getLastPeladaActive: jest.fn(() =>
+			Promise.resolve(Pelada.createPelada('place', 20, 10, new Date(), new Player(1, true))),
+		),
+	} as IPeladaRepository;
+	return new PeladaService(peladaRepository);
+};
 
 describe('PeladaService', () => {
 	it('create pelada should throw exception when command is invalid', () => {
-		const sut = new PeladaService();
-		expect(() => sut.createPelada('.criar')).toThrowError(
+		const sut = createSut();
+		expect(sut.createPelada('.criar')).rejects.toThrowError(
 			'Para criar uma pelada, utilize o comando: .criar <nome> <data> <horario>',
 		);
 	});
 
-	it('createPelada should throw exception when commands has date is before than last active pelada', () => {
-		const sut = new PeladaService();
-		expect(() => sut.createPelada('.criar 01/32/2020 qualquerLocal 19:00')).toThrowError('Data inválida');
+	it('should throw when admin tries to create one pelada but there is already one running', () => {
+		const sut = createSut();
+		expect(sut.createPelada('.criar 31/02/2020 qualquerLocal 19:00')).rejects.toThrow('Já existe uma pelada rolando!');
 	});
 });
