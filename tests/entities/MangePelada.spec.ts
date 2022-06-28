6ec7c17f8718ee1ee996fe9a374a034c67b13e4c
Victor Emídio <victoremidiO@yahoo.com.br>
2022-06-27 21:40:53 -0300

    refactor: remove unused usecase

--- tests/entities/MangePelada.spec.ts
+++ tests/entities/MangePelada.spec.ts
@@ -1,17 +1,10 @@
-import Pelada from '@/entities/Pelada';
-import Player from '@/entities/Player';
+import Pelada from '@/domain/entities/Pelada';
+import Player from '@/domain/entities/Player';
 
 describe('CreatePelada', () => {
 	it('should throw exception if the player is not an admin', () => {
 		expect(() => {
 			Pelada.createPelada('place', 20, 10, new Date(), new Player(1, false));
 		}).toThrowError('Você não tem permissão para criar uma pelada');
-	});
-
-	it('should throw when admin tries to create with date before than today', () => {
-		const admin = new Player(1, true);
-		expect(() => {
-			Pelada.createPelada('place', 20, 10, new Date(2000, 1, 1), admin);
-		}).toThrowError('Você não pode criar uma pelada com a data anterior ao dia atual!');
 	});
 });
