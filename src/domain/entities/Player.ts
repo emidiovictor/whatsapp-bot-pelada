9510d94ba8a031785c4ade123006a59b9ff541ff
Victor Emídio <victoremidiO@yahoo.com.br>
2022-06-28 22:11:35 -0300

    refactor: style

--- src/domain/entities/Player.ts
+++ src/domain/entities/Player.ts
@@ -1,7 +1,12 @@
 import Pelada from './Pelada';
 
 export class Player {
-	constructor(readonly id: number, readonly isAdmin: boolean = false, readonly peladaOwner?: Pelada) {}
+	constructor(
+		readonly phoneNumber: string,
+		readonly name: string,
+		readonly isAdmin: boolean = false,
+		readonly peladaOwner?: Pelada,
+	) {}
 }
 
 export default Player;
