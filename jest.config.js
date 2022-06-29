c60478151db9ed3dfa40f46c9f00411c5a365306
Victor Emídio <victoremidiO@yahoo.com.br>
2022-06-28 22:13:03 -0300

    refactor: style

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,15 @@
-
 module.exports = {
-  roots: ['<rootDir>/tests'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
-  ],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
-    '@/(.*)': '<rootDir>/src/$1'
-  }
-}
+	roots: ['<rootDir>/tests'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+	coverageDirectory: 'coverage',
+	coverageProvider: 'babel',
+	testEnvironment: 'node',
+	transform: {
+		'.+\\.ts$': 'ts-jest',
+	},
+	moduleNameMapper: {
+		'@/tests/(.*)': '<rootDir>/tests/$1',
+		'@/(.*)': '<rootDir>/src/$1',
+	},
+	moduleDirectories: ['node_modules', 'src'],
+};
